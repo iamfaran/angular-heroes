5e1d40c3e3319592b30409345d552cdaf6e78d90
Faran Javed <faran1997@outlook.com>
2023-09-30 16:26:50 +0500

    use service in heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor() {
+  constructor(private heroService: HeroService) {
     console.log(this);
   }
   onHeroClick(hero: Hero): void {
     this.selectedHero = hero;
     console.log('Parent component HAHAH', this.selectedHero);
+  }
+
+  getHeroes() {
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
