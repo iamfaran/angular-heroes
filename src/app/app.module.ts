b3b490b2fb86d8a9d7402283880212017855f5af
Faran Javed <faran1997@outlook.com>
2023-09-30 15:48:23 +0500

    create dummy detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroComponent } from './hero/hero.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroComponent],
+  declarations: [AppComponent, HeroesComponent, HeroComponent, HeroDetailComponent],
   imports: [
     BrowserModule,
     FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
