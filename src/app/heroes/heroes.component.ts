86809be9d8a9d99fadf42f622f87d24f591bc6db
Faran Javed <faran1997@outlook.com>
2023-10-01 17:13:41 +0500

    display selected hero id in messages

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,12 +11,16 @@ export class HeroesComponent {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {
     console.log(this);
   }
   onHeroClick(hero: Hero): void {
     this.selectedHero = hero;
     console.log('Parent component HAHAH', this.selectedHero);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes() {
